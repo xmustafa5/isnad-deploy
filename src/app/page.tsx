afdee76ea53c9889737c46a1f6ed3a429b0a9f0c
mustafa <musmoh73@gmail.com>
2025-05-11 20:52:03 +0300

    Enhance button component with additional variants and update global styles for improved design consistency. Modify page layout to incorporate new button configurations and adjust CSS variables for better clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,18 @@ import { Button } from "@/components/Button";
 
 export default function Home() {
   return ( 
-    <div className=" body-14-medium">
-      <Button variant="default" size="lg">Click me</Button>
+    <div className="bg-black w-full grid grid-cols-3 gap-4 justify-center items-center h-screen">
+      <Button variant="default" mode="default">Click me</Button>
+      <Button variant="default" mode="outline">Click me</Button>
+      <Button variant="default" mode="text">Click me</Button>
+      <Button variant="error" mode="default">Click me</Button>
+      <Button variant="error" mode="outline">Click me</Button>
+      <Button variant="error" mode="text">Click me</Button>
+      <Button variant="gray"  >Click me</Button>
+      <Button variant="gray" >Click me</Button>
+      <Button variant="gray">Click me</Button>
+     
+      
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,38 +5,54 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "flex w-[252px] justify-center items-center gap-2 flex-shrink-0 rounded-md",
+  "flex w-[252px] justify-center items-center text-text-whaite gap-2 flex-shrink-0 rounded-md backdrop-blur-[16px] border",
   {
     variants: {
       variant: {
         default:
-          "bg-primary-60 bord",
+          "bg-primary-60 border-primary-60 ",
         error:
-          "bg-text-error",
+          "bg-text-error border-stroke-error ",
         gray:
-          "",
+          "border-opacity-white-10",
         buttons:
           "",
       },
       mode: {
         default:
-          " ",
+          "",
         outline:
-          "",
+          "bg-transparent text-primary-60",
         text:
-          "",
-
+          "bg-transparent border-transparent ",
       },
       size: {
-        default: "h-[56px] py-4",
-        lg: "h-[48px] py-3",
+        m: "h-[48px] py-3",
+        lg: "h-[56px] py-4",
         s: "h-[40px] py-3",
       },
     },
+    compoundVariants: [
+      {
+        variant: "error",
+        mode: "outline",
+        className: "bg-transparent text-text-error",
+      },
+      {
+        variant: "error",
+        mode: "text",
+        className: "text-text-error",
+      },
+      {
+        variant: "default",
+        mode: "text",
+        className: "text-primary-60",
+      },
+    ],
     defaultVariants: {
       variant: "default",
       mode: "default",
-      size: "default",
+      size: "m",
     },
   }
 )
@@ -48,11 +64,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, mode, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, mode, className }))}
         ref={ref}
         {...props}
       />
