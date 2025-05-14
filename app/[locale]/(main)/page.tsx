e0f6d75bad4c92838cd1dfa5c093ba38e44c5378
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2025-05-14 19:37:55 +0300

    Enhance global styles with a new background for the login page and update typography utilities. Refactor the main page layout by removing redundant button components. Update the sign-in page to include a structured layout with new buttons for improved user experience.

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from "@/app/_components/Button";
 
 export default function Home() {
   return (
     <div className="bg-black w-full grid grid-cols-3 gap-4 justify-center items-center h-screen">
-
-      <Button variant="buttons">Click me</Button>
-      <Button variant="buttons" >Click me</Button>
-      <Button variant="buttons">Click me</Button>
-
-
 
     </div>
   );
--- app/[locale]/auth/signin/page.tsx
+++ app/[locale]/auth/signin/page.tsx
@@ -1,20 +1,25 @@
-
-
-
-
+import { Button } from "@/app/_components/Button";
+import { mobilelogin } from "@/public/svg/login";
 
 function Page() {
-
-
-
-
-
-
-
-
   return (
-    <div>
-      اثممخ
+    <div className="background-login h-[100dvh] flex justify-center items-end">
+      <div
+        className="w-full absolute top-0 left-0 h-full flex-shrink-0 pointer-events-none"
+        style={{ background: 'linear-gradient(180deg, rgba(8, 24, 47, 0.00) 50%, #08182F 91.19%)' }}
+      ></div>
+      <div className="flex flex-col items-center gap-[29px] w-[398px] ">
+        <div className="flex flex-col items-start gap-4 w-[341.273px]">
+          <p className="self-stretch text-center typography-title-24-light text-white">Finding your ideal property has never been easier</p>
+          <p className="self-stretch text-center typography-body-14-medium text-white">Sign in to your account to continue</p>
+        </div>
+        <div className="flex flex-col items-start gap-4 self-stretch">
+          <Button size="lg" className="w-full">Sign up with phone number {mobilelogin}</Button>
+          <Button variant="gray" size="lg" className="w-full">Sign up with Google</Button>
+          <Button variant="default" mode="text" size="lg" className="w-full">Login as a guest</Button>
+        </div>
+        <div className="flex flex-col items-start gap-4 self-stretch"></div>
+      </div>
     </div>
   );
 }
