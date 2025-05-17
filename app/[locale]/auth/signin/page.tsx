322242441721a796e6c3945d60000006d8b9e412
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2025-05-17 12:01:04 +0300

    Refactor sign-in page layout to enhance responsiveness and user experience. Update component structures for Step1 and Step2, ensuring consistent styling and improved button interactions. Adjust input field styles for better accessibility and visual clarity.

--- app/[locale]/auth/signin/page.tsx
+++ app/[locale]/auth/signin/page.tsx
@@ -15,14 +15,21 @@ function Page() {
             `linear-gradient(180deg, rgba(8, 24, 47, ${step > 1 ? "0.10" : "0.00"}) ${step > 1 ? "0%" : "50%"},  ${step > 1 ? "rgba(8, 24, 47, 0.90) 54.18%" : "#08182F 91.19%"} )`,
         }}
       ></div>
-      {step === 1 ? <Step1 setStep={setStep} /> : <Step2 />}
+      <div className="flex flex-col justify-between relative md:justify-center h-full min-w-[398px] md:max-w-[494px]">
+        <div className="w-full flex flex-col h-full ">
+          <div className="w-full h-full absolute "></div>
+          <div className="flex justify-between bg-red-500  items-center h-full md:h-fit min-w-[398px] relative z-10">
+            {step === 1 ? <Step1 setStep={setStep} /> : <Step2 />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 function Step1({ setStep }: { setStep: (step: number) => void }) {
   return (
-    <>
-      <div className="flex justify-between items-center w-[398px] h-[47px] relative z-10">
+    <div className="flex flex-col justify-between md:justify-center h-full md:h-fit items-center w-full">
+      <div className="flex justify-between  md:hidden items-center h-[47px] w-full relative z-10">
         <Component2>
           {NotificationSvg}
         </Component2>
@@ -31,7 +38,7 @@ function Step1({ setStep }: { setStep: (step: number) => void }) {
           {burgerMenuSvg}
         </Component2>
       </div>
-      <div className="flex flex-col items-center gap-[29px] w-[398px] relative z-10 ">
+      <div className="flex flex-col items-center gap-[29px] w-full  relative z-10 ">
         <div className="flex flex-col items-start gap-4 w-[341.273px]">
           <p className="self-stretch text-center typography-title-24-light text-white">
             Finding your ideal property has never been easier
@@ -52,13 +59,13 @@ function Step1({ setStep }: { setStep: (step: number) => void }) {
           </Button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 function Step2() {
   return (
-    <div className="relative z-10">
-      <div className="flex flex-col items-end gap-2 w-[356px]">
+    <div className="relative z-10 flex flex-col items-center justify-center w-full h-full">
+      <div className="flex flex-col items-end gap-2 w-[356px] mb-[33px]">
         <p className="self-stretch typography-title-28-light text-white">
           Enter your phone number
         </p>
@@ -67,12 +74,20 @@ function Step2() {
         </p>
       </div>
       <InputPhoneNumber />
+      <div className="flex flex-col gap-4 mt-[48px] min-w-[398px]">
+        <Button size="lg" className="w-full">
+          Send via WhatsApp.
+        </Button>
+        <Button size="lg" variant="gray" className="w-full">
+          Send code
+        </Button>
+      </div>
     </div>
   )
 }
 function InputPhoneNumber() {
   return (
-    <div className="flex w-[398px] flex-col gap-[-1]">
+    <div className="flex min-w-[398px] flex-col gap-[-1]">
       <div className="flex relative items-center justify-end gap-2 px-5 py-2 rounded-t-lg bg-white w-fit">
         <p className="text-right text-[14px] font-medium leading-[140%] text-[#08182F] font-[Alilato]">
           IRAQ
@@ -83,13 +98,15 @@ function InputPhoneNumber() {
         </svg>
       </div>
       <div className="relative flex items-center gap-[18px] px-2 py-3 self-stretch rounded-br-[19px] rounded-bl-[19px] rounded-tr-[19px] border border-white/25 ">
-        <div className="absolute inset-0 backdrop-blur-[65px] rounded-br-[19px] rounded-bl-[19px] rounded-tr-[19px] z-[1]"></div>
-        <div className="flex items-center justify-center gap-2 px-4 py-2">
+        <div className="absolute inset-0 pointer-events-none backdrop-blur-[65px] rounded-br-[19px] rounded-bl-[19px] rounded-tr-[19px] z-[1]"></div>
+        <div className="flex items-center justify-center gap-2 relative z-10 px-4 py-2 border-r border-[rgba(233,235,240,0.06)]">
           <p className="text-right text-[18px] font-medium leading-[140%] text-white relative z-10 font-[Alilato]">
             +964
           </p>
         </div>
+        <input type="text" className=" relative z-10 text-[#fff] focus:outline-none font-[Alilato] text-[20px] not-italic font-semibold leading-[140%] placeholder:text-[#AEBACD]" />
       </div>
+
     </div>
   )
 }
